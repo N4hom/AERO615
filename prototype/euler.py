ad475c65185d7377c19a62ed6ef11b8883120579
N4hom <h.nahom10@gmail.com>
2024-04-23 14:15:46 -0500

    inlet corrected in python

--- prototype/euler.py
+++ prototype/euler.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.set_printoptions(precision=5, suppress=True)
+
 
 def delta2(q , i ):
-	
+	print("q[i + 1] " , q[i + 1])
+	print("q[i    ] " , q[i    ])
+	print("q[i - 1] " , q[i - 1])
 	return  q[i + 1] - 2 * q[i] + q[i - 1]
 
 def interpolateLeft(q , i ):
@@ -115,17 +117,29 @@ class variable1D(object):
 		# print("lambda west " , lambdaFace.w)
 
 		for i in range(self.Nx):
-			deltaqw  = self.field[i ] - self.field[i - 1]
-			delta3qw = delta2(self.field , i) - delta2(self.field , i - 1)
-			deltaqe  = self.field[i + 1] - self.field[i ]
-			delta3qe = delta2(self.field , i + 1) - delta2(self.field , i )
-			self.D.w[i] = s2face.w[i] * deltaY * lambdaFace.w[i] * deltaqw - s4face.w[i] * deltaY * lambdaFace.w[i] * delta3qw
-			self.D.e[i] = s2face.e[i] * deltaY * lambdaFace.e[i] * deltaqe - s4face.e[i] * deltaY * lambdaFace.e[i] * delta3qw
+			ic = i + 2
+			print("i " , i)
+			deltaqw  = self.field[ic] - self.field[ic - 1]
+			self.print()
+			delta3qw = delta2(self.field , ic) - delta2(self.field , ic - 1)
+			print("delta3qw " , delta3qw )
+			delta3qw = (self.field[ic + 1 ] - 2*self.field[ic] + self.field[ic - 1]) - (self.field[ic] - 2*self.field[ic - 1] + self.field[ic - 2])
+			print("delta3qw " , delta3qw )
+			print("s4face.w[i] " , s4face.w[i] )
+			print("lambdaFace.w[i] " , lambdaFace.w[ic] )
+			print("s4face.w[i] * deltaY * lambdaFace.w[i] * delta3qw " , s4face.w[i] * deltaY * lambdaFace.w[ic] * delta3qw)
+			deltaqe  = self.field[ic + 1] - self.field[ic]
+			delta3qe = delta2(self.field , ic + 1) - delta2(self.field , ic)
+			print("delta3qe " , delta3qe )
+			delta3qe = (self.field[ic + 2] - 2*self.field[ic + 1] + self.field[ic ]) - (self.field[ic + 1] - 2*self.field[ic] + self.field[ic - 1])
+			print("delta3qe " , delta3qe )
+			self.D.w[i] = s2face.w[i] * deltaY * lambdaFace.w[ic] * deltaqw - s4face.w[i] * deltaY * lambdaFace.w[ic] * delta3qw
+			self.D.e[i] = s2face.e[i] * deltaY * lambdaFace.e[ic] * deltaqe - s4face.e[i] * deltaY * lambdaFace.e[ic] * delta3qe
 			self.D.p[i] = self.D.e[i] - self.D.w[i]
 
-		# print(self.D.w)
-		# print(self.D.e)
-		# print(self.D.p)
+		print("self.D.w " , self.D.w)
+		print("self.D.e " , self.D.e)
+		print("self.D.p " , self.D.p)
 
 
 	def computeFluxij(self , u , p , i):
@@ -187,9 +201,9 @@ class Euler(object):
 		self.pInf = pInf
 		self.rhoInf = rhoInf
 		self.pRatio = pRatio
-		self.M = Mach
+		self.Minf = Mach
 		self.cInf = np.sqrt(1.4 * self.pInf / self.rhoInf)
-		self.uInf = self.M * self.cInf
+		self.uInf = self.Minf * self.cInf
 		self.gamma = gamma
 		self.gamma_1 = gamma - 1
 
@@ -200,16 +214,16 @@ class Euler(object):
 
 
 
-		self.rho = variable1D  ("rho"   , Nx , 0.0 , 0.0 , self.rhoInf * 0.1 )
+		self.rho = variable1D  ("rho"   , Nx , 0.0 , 0.0 , self.rhoInf  )
 		self.rhoU = variable1D ("rhoU"  , Nx , 0.  , 0.  , self.rhoInf * self.uInf  )  
 	#	self.rhoV = variable1D ("rhoV"  , Nx , 1.  , 0.  ,  )
 		self.p = variable1D    ("p"     , Nx , 0.  , 0.  , self.pInf * self.pRatio) 
 		self.rhoE = variable1D ("rhoE"  , Nx , 0.  , 0.  , self.pInf * self.pRatio/self.gamma_1 + 0.5 * self.rhoInf * self.uInf * self.uInf )
 		self.u = variable1D    ("u"     , Nx , 0.  , 0.  , self.uInf )
 		self.c = variable1D    ("c"     , Nx , 0.  , 0.  , self.cInf ) # assuming p and rho are inf
-		self.M = variable1D    ("M"     , Nx , 0.  , 0.  , self.M)
+		self.M = variable1D    ("M"     , Nx , 0.  , 0.  , self.Minf)
 		self.Riem1 = variable1D("Riem1" , Nx , 0.  , 0.  , self.uInf + 2/self.gamma_1 * self.cInf )
-		self.Riem2 = variable1D("Riem2" , Nx , 0.  , 0.  ,  )
+		self.Riem2 = variable1D("Riem2" , Nx , 0.  , 0.  , self.uInf + 2/self.gamma_1 * self.cInf  )
 
 	
 	def correctInlet(self):
@@ -226,33 +240,50 @@ class Euler(object):
 		uInf = self.uInf
 		cInf = self.cInf
 		M = self.M
+		Minf = self.Minf 
 		pInf = self.pInf
+		rhoInf = self.rhoInf
 
 		print("Correct inlet ")
 
-		Riem1[1] = uInf + 2/gamma_1 * cInf
-		Riem2[1] = u[2] - 2/gamma_1 * c[2]  # Riem2[1] = Riem2[2]  
+		Riem1[2] = uInf + 2/gamma_1 * cInf   # i = 2 corresponds to i = 1 in the notes
+		Riem2[2] = u[3] - 2/gamma_1 * c[3]  # Riem2[1] = Riem2[2]  
 
-		u[1] = 0.5 * (Riem1[1] + Riem2[1])
-		u[0] = u[1]
+		print("Riem1 " , Riem1.field)
+		print("Riem2 " , Riem2.field)
+
+		u[2] = 0.5 * (Riem1[2] + Riem2[2])
+
+
+		u[0] = uInf
+		u[1] = uInf
 		
-		c[1] = 0.25 * gamma_1 * (Riem1[1] - Riem2[1])
-		c[0] = c[1]
+		c[2] = 0.25 * gamma_1 * (Riem1[2] - Riem2[2])
+		c[0] = cInf
+		c[1] = cInf
 
-		M[1] = u[1]/c[1]
-		M[0] = M[1]
+		M[2] = u[2]/c[2]
+		print("M[2] " , M[2])
+		M[0] = Minf
+		M[1] = Minf
 
-		p[1] = pInf/(1 + 0.5 * gamma_1 * M[1]**2)**(gamma/gamma_1)
-		p[0] = p[1]
+		p[2] = pInf/(1 + 0.5 * gamma_1 * M[2]**2)**(gamma/gamma_1)
+		print("pInf/(1 + 0.5 * gamma_1 * M[2]**2)**(gamma/gamma_1) " , pInf/(1 + 0.5 * gamma_1 * M[2]**2)**(gamma/gamma_1))
+		p[0] = pInf
+		p[1] = pInf
 
-		rho[1] = gamma * p[1]/c[1]**2
-		rho[0] = rho[1]
+		rho[2] = gamma * p[2]/c[2]**2
+		print("	rho[2] = gamma * p[2]/c[2]**2 " , gamma * p[2]/c[2]**2)
+		rho[1] = rhoInf
+		rho[0] = rhoInf
 
-		rhoU[1] = rho[1] * u[1]
-		rhoU[0] = rhoU[1]
+		rhoU[2] = rho[2] * u[2]
+		rhoU[0] = rhoInf * uInf
+		rhoU[1] = rhoInf * uInf
 
+		rhoE[2] = p[2]/gamma_1 + 0.5 * rho[2] * u[2]**2
+		rhoE[0] = p[0]/gamma_1 + 0.5 * rho[0] * u[0]**2
 		rhoE[1] = p[1]/gamma_1 + 0.5 * rho[1] * u[1]**2
-		rhoE[0] = rhoE[1]
 
 
 	def correctOutlet(self):
@@ -269,6 +300,7 @@ class Euler(object):
 		uInf = self.uInf
 		cInf = self.cInf
 		M = self.M
+		Minf = self.Minf 
 		pInf = self.pInf
 		pRatio = self.pRatio
 		icMax = self.icMax
@@ -326,8 +358,10 @@ class Euler(object):
 		nu4 = 0.001
 		p = self.p
 
-		for i in range(1,len(p.field)-1):
-			self.s2[i] = 0.1 * abs(delta2(p , i))/(p[i + 1] + 2*p[i] + p[i - 1]) 
+		for i in range(1,self.Nx):
+			print("delta2(p , i) " , delta2(p , i))
+			self.s2[i] = nu2 * abs(delta2(p , i))/(p[i + 1] + 2*p[i] + p[i - 1]) 
+
 
 		print("s2 " ,self.s2)
 		# Interpolate switches
@@ -342,6 +376,10 @@ class Euler(object):
 			self.s4face.w[i] = max(0 , nu4 - self.s2face.w[i])
 			self.s4face.e[i] = max(0 , nu4 - self.s2face.e[i])
 
+
+		print(self.s4face.w)
+		print(self.s4face.e)
+
 			
 	def computeEigen(self ):
 		print("computeEigen")
@@ -349,8 +387,12 @@ class Euler(object):
 
 		for i in range(self.Nx):
 			ic = i + 2
-			self.lambdaFace.w[ic] = interpolateLeft(self.u.field , ic) + interpolateLeft(self.c.field ,ic)
-			self.lambdaFace.e[ic] = interpolateRight(self.u.field , ic) + interpolateRight(self.c.field , ic)
+			self.lambdaFace.w[ic] = interpolateLeft(self.u.field , ic) + self.c.field[ic]
+			self.lambdaFace.e[ic] = interpolateRight(self.u.field , ic) + self.c.field[ic]
+
+		print(self.lambdaFace.w)
+
+
 
 
 
@@ -384,31 +426,34 @@ class Euler(object):
 		alpha3 = self.alpha3
 		alpha4 = self.alpha4
 		
-		dt = 1e-6
+		dt = 1e-3
 
 		ic = i + 2
 		variable0 = variable[ic].copy()
+		D0 = variable.D.p[i].copy()
 
-		# if variable.name == "rho":
-		# 	print("variable.R.p[i] " , variable.R.p[i])
-		# 	print("variable.D.p[i] " , variable.D.p[i])
-		# 	print("variable[ic] before" , variable.field[ic])
-		variable[i] = variable0 - alpha1 * dt / (self.Lx/self.Nx) * (variable.R.p[i]  - variable.D.p[i])
+		if variable.name == "rho":
+			print("i " , i)
+			print("variable.R.p[i] " , variable.R.p[i])
+			print("variable.D.p[i] " , variable.D.p[i])
+			print("variable[ic] before" , variable.field[ic])
+
+		variable[ic] = variable0 - alpha1 * dt / (self.Lx/self.Nx) * (variable.R.p[i]  - variable.D.p[i])
 		variable.computeFluxij(self.u , self.p , ic)
 		variable.computeResidualij(i)
 		
-		variable[i] = variable0 - alpha2 * dt / (self.Lx/self.Nx) * (variable.R.p[i]  - variable.D.p[i])
+		variable[ic] = variable0 - alpha2 * dt / (self.Lx/self.Nx) * (variable.R.p[i]  - variable.D.p[i])
 		variable.computeFluxij(self.u , self.p , ic)
 		variable.computeResidualij(i)
 		
-		variable[i] = variable0 - alpha3 * dt / (self.Lx/self.Nx) * (variable.R.p[i]  - variable.D.p[i])
+		variable[ic] = variable0 - alpha3 * dt / (self.Lx/self.Nx) * (variable.R.p[i]  - variable.D.p[i])
 		variable.computeFluxij(self.u , self.p , ic)
 		variable.computeResidualij(i)
 		
 		variable[ic] = variable0 - alpha4 * dt / (self.Lx/self.Nx) * (variable.R.p[i]  - variable.D.p[i]) # Only one step for testing
 
-		# if variable.name == "rho":
-		# 	print("variable[ic] after" , variable[ic])
+		if variable.name == "rho":
+			print("variable[ic] after" , variable[ic])
 		
 
 		# variable[i] = variable0 - alpha3 * dt / (self.Lx/self.Nx) * variable.R.p[i]
@@ -431,7 +476,7 @@ class Euler(object):
 		
 
 		it = 0
-		N  = 5000
+		N  = 100
 		while it < N:
 
 			print("-----------------------------")
@@ -440,7 +485,11 @@ class Euler(object):
 			self.rho.print()
 			self.rhoU.print()
 			self.rhoE.print()
+			self.u.print()
 			self.p.print()
+			self.c.print()
+			self.Riem1.print()
+			self.Riem2.print()
 
 			print()
 			self.rho.printFlux()
@@ -486,22 +535,22 @@ class Euler(object):
 
 			rho0 = self.rho.field.copy()
 
-			# loop over internal field
-			for i in range(len(self.rho.R.p)):
-				# print(i)
-				# print("R rho" , self.rho.R.p)
-				# print("D rho" , self.rho.D.p)
+			for i in range(1,len(self.rho.R.p)):
 				self.RungeKutta(self.rho , i)			
-				# print("R rhoU" , self.rhoU.R.p)
+
+			for i in range(1,len(self.rho.R.p)):
 				self.RungeKutta(self.rhoU , i)
-				# print("R rhoE" , self.rhoE.R.p)
+			for i in range(1,len(self.rho.R.p)):
 				self.RungeKutta(self.rhoE , i)
 
 			self.rho.residual.append(abs(rho0[3] - self.rho.field[3]))
 
-			# self.rho.print()
-			# self.rhoU.print()
-			# self.rhoE.print()
+			self.rho.print()
+			self.rhoU.print()
+			self.rhoE.print()
+			self.u.print()
+			self.Riem1.print()
+			self.Riem2.print()
 
 
 			self.correctFields()
@@ -523,7 +572,7 @@ class Euler(object):
 		
 	
 
-problem = Euler(4 ,  1e-9 , 1 , 0.1 , 0.3 , 1e5 , 0.9 , 1)
+problem = Euler(10 ,  1e-9 , 1 , 0.1 , 0.3 , 1e5 , 0.9 , 1)
 
 problem.solve()
 
